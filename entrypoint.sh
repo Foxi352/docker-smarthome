a0281e30869ebba0cbb24046b77a960018fb4b0e
Foxi352 <serge@swa.lu>
2017-05-22 14:41:03 +0200

    Corrected logic.yaml filename, improved mysql=yes detection, added telepot python lib for telegram plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ SHNG_PATH=/usr/local/smarthome
 EXAMPLES_PATH=/usr/local/examples
 
 # If MySQL is enabled wait for it to be up and our database to be ready 
-if [ ! -z "$SHNG_USE_MYSQL" ]; then
+if [ "$SHNG_USE_MYSQL" = "yes" ]; then
 	echo "Waiting for MySQL database '$MYSQL_DATABASE' on host '$SHNG_MYSQL_HOST'"
 	./check_for_mysql.py --host=$SHNG_MYSQL_HOST --database=$MYSQL_DATABASE --user=$MYSQL_USER --password=$MYSQL_PASSWORD
 	while [ $? -ne 0 ] ; do
@@ -22,8 +22,8 @@ fi
 if [ ! -e "$SHNG_PATH/etc/logging.yaml" ]; then 
     cp $EXAMPLES_PATH/etc/logging.yaml $SHNG_PATH/etc/logging.yaml; 
 fi
-if [ ! -e "$SHNG_PATH/etc/logics.yaml" ]; then 
-    cp $EXAMPLES_PATH/etc/logics.yaml $SHNG_PATH/etc/logics.yaml; 
+if [ ! -e "$SHNG_PATH/etc/logic.yaml" ]; then 
+    cp $EXAMPLES_PATH/etc/logic.yaml $SHNG_PATH/etc/logic.yaml; 
 fi
 if [ ! -e "$SHNG_PATH/etc/plugin.yaml" ]; then 
     cp $EXAMPLES_PATH/etc/plugin.yaml $SHNG_PATH/etc/plugin.yaml; 
